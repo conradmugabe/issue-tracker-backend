2f1a8f6986dec3f2a6312af2d76f838f4fbd7e72
conrad mugabe <conradmugabe@gmail.com>
2024-10-08 12:09:20 +0300

    docs: added readme file

--- use_cases/__init__.py
+++
@@ -1,7 +0,0 @@
-"""use cases"""
-
-from use_cases.issues import IssuesUseCases
-from storage.issues.in_memory_db import TodoInMemoryDatabaseService
-
-database_service = TodoInMemoryDatabaseService()
-issues_use_cases = IssuesUseCases(database_service=database_service)
